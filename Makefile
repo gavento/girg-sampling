778f075fc7ac4aa9b4dd3b17ff2e45be3c07166b
Tomas Gavenciak <gavento@gmail.com>
2020-10-13 13:20:25 +0200

    Working build script and poetry build

--- Makefile
+++ Makefile
@@ -1,10 +1,31 @@
-.PHONY: clean
+LIB_BUILD_DIR=girgs_cpplib/build
+LIB_TARGET_DIR=girgs_cpplib/install
+LIB_GIRGS=girg_sampling/libgirgs.so
+LIB_WRAP=girg_sampling/_cpplib_wrapper.so
+
+PYBIND11_INCLUDE=$(shell poetry run python -c "import pybind11; print(pybind11.get_include())")
+PYTHON_INCLUDE=$(shell poetry run python -c "from sysconfig import get_paths; print(get_paths()['include'])")
+
+.PHONY: all clean build
+
+all: build
 
 clean:
-	rm dist/ build/ girg_sampling/_girgs_cpplib -rf
+	rm -rf dist/ build/ $(LIB_BUILD_DIR) $(LIB_TARGET_DIR) girg_sampling/*.so
+	rm -rf girg_sampling.egg-info/ __pycache__/
 
-build:
-	poetry build
+$(LIB_GIRGS):
+	cmake girgs_cpplib -B $(LIB_BUILD_DIR)
+	cmake --build $(LIB_BUILD_DIR)
+	cmake --install $(LIB_BUILD_DIR) --prefix $(LIB_TARGET_DIR)
+	cp $(LIB_TARGET_DIR)/lib/*.so girg_sampling/
+
+$(LIB_WRAP): girg_sampling/_cpplib_wrapper.cpp $(LIB_GIRGS)
+	$(CC) -fPIC -Wall -shared -g -o $@ $< -lstdc++ \
+		-I$(PYBIND11_INCLUDE) -I$(LIB_TARGET_DIR)/include/ -I$(PYTHON_INCLUDE)
+
+build: $(LIB_WRAP)
+#	poetry build
 
 test:
 	poetry install
--- build.py
+++ build.py
@@ -1,21 +1,42 @@
-from cmaketools.cmakebuilder import CMakeBuilder
 #from cmaketools.cmakecommands import generate_cmdclass
-#from cmaketools import setup
+#from cmaketools.cmakebuilder import CMakeBuilder
+#import pybind11
+import subprocess
+#from distutils.command.build_ext import build_ext, Extension
 
+"""
+ext_modules = [
+    Extension(
+        "girg_sampling._cpplib_wrapper",
+        include_dirs=[pybind11.get_include(), "dist/include/"],
+        sources=["girg_sampling/_cpplib_wrapper.cpp"],
+    ),
+]
+"""
 
-def build():
+def build(setup_kwargs):
     print("Heelo world!")
-    cmake = CMakeBuilder()
-    cmake.configure(build_dir="build/girgs_cpplib")
-    cmake.run("girg_sampling/_girgs_cpplib")
 
-#    setup_kwargs["packages"] = cmake.find_packages()
-#    setup_kwargs["ext_modules"] = cmake.find_ext_modules()
-#    setup_kwargs["data_files"] = cmake.get_setup_data_files()
-#    setup_kwargs["cmdclass"] = {
-#        **generate_cmdclass(cmake),
-#    }
+    #    setup_kwargs["ext_module_hint"] = r"pybind11_add_module"
+    #    setup_kwargs["src_dir"] = "girgs_cpplib"
+
+    #cmake = CMakeBuilder(src_dir=".")
+    #cmake.configure(build_dir="build/girgs_cpplib")
+    #cmake.run("girg_sampling/_girgs_cpplib")
+
+    #subprocess.check_call(["gcc", "-c", "girg_sampling/_cpplib_wrapper.cpp"])
+    subprocess.check_call(["make", "build"])
+
+    #setup_kwargs["packages"] = cmake.find_packages()
+    #setup_kwargs["ext_modules"] = cmake.find_ext_modules()
+    #setup_kwargs["data_files"] = cmake.get_setup_data_files()
+    #setup_kwargs["cmdclass"] = {
+    #    **generate_cmdclass(cmake),
+    #}
+    #setup_kwargs["ext_modules"] += ext_modules
+
+    return setup_kwargs
 
 
 if __name__ == "__main__":
-    build()
+    build({})
