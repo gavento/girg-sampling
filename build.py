c7a62f69646317c747650e51c3cc364fd0de7eaf
Tomas Gavenciak <gavento@gmail.com>
2020-10-15 14:23:16 +0200

    Fix files with pre-commit

--- build.py
+++ build.py
@@ -1,8 +1,9 @@
-#from cmaketools.cmakecommands import generate_cmdclass
-#from cmaketools.cmakebuilder import CMakeBuilder
-#import pybind11
+# from cmaketools.cmakecommands import generate_cmdclass
+# from cmaketools.cmakebuilder import CMakeBuilder
+# import pybind11
 import subprocess
-#from distutils.command.build_ext import build_ext, Extension
+
+# from distutils.command.build_ext import build_ext, Extension
 
 """
 ext_modules = [
@@ -14,26 +15,27 @@ ext_modules = [
 ]
 """
 
+
 def build(setup_kwargs):
     print("Heelo world!")
 
     #    setup_kwargs["ext_module_hint"] = r"pybind11_add_module"
     #    setup_kwargs["src_dir"] = "girgs_cpplib"
 
-    #cmake = CMakeBuilder(src_dir=".")
-    #cmake.configure(build_dir="build/girgs_cpplib")
-    #cmake.run("girg_sampling/_girgs_cpplib")
+    # cmake = CMakeBuilder(src_dir=".")
+    # cmake.configure(build_dir="build/girgs_cpplib")
+    # cmake.run("girg_sampling/_girgs_cpplib")
 
-    #subprocess.check_call(["gcc", "-c", "girg_sampling/_cpplib_wrapper.cpp"])
+    # subprocess.check_call(["gcc", "-c", "girg_sampling/_cpplib_wrapper.cpp"])
     subprocess.check_call(["make", "build"])
 
-    #setup_kwargs["packages"] = cmake.find_packages()
-    #setup_kwargs["ext_modules"] = cmake.find_ext_modules()
-    #setup_kwargs["data_files"] = cmake.get_setup_data_files()
-    #setup_kwargs["cmdclass"] = {
+    # setup_kwargs["packages"] = cmake.find_packages()
+    # setup_kwargs["ext_modules"] = cmake.find_ext_modules()
+    # setup_kwargs["data_files"] = cmake.get_setup_data_files()
+    # setup_kwargs["cmdclass"] = {
     #    **generate_cmdclass(cmake),
-    #}
-    #setup_kwargs["ext_modules"] += ext_modules
+    # }
+    # setup_kwargs["ext_modules"] += ext_modules
 
     return setup_kwargs
 
